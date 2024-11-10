f67183b9d173f54f8173c51826edc2667fabd19d
Christian Medallada <cbmedallada@student.apc.edu.ph>
2024-11-11 00:50:52 +0800

    configure the game.py

--- mygame/game.py
+++ mygame/game.py
@@ -1,75 +1,50 @@
 import random
+import argparse
 from string import capwords
 
-print("Lets Play Rock Paper Scissors")
-
-def Main():
-    
+def play_game(player_choice):
+    print("Let's Play Rock Paper Scissors")
     Choices = ["Rock", "Paper", "Scissors"]
-  
+
     print("""Rules: 
-    \nYou should only Pick"""+ "\n")
+    \nYou should only Pick""")
     for c in Choices:
         print(c)
-    
-    
-    People = input("\nEnter your Choice here: ")
-    Computer = random.choice(Choices)
-    People = capwords(People)
 
-    
-    print("\nYour Pick is: " + People)
-    print("Computer pick is: " + Computer + "\n")
+    player_choice = capwords(player_choice)
+    computer_choice = random.choice(Choices)
 
-    if People == Computer:
-        print("The pick is draw")
-    elif People == "Rock":
-        if Computer == "Scissors":
+    print("\nYour Pick is: " + player_choice)
+    print("Computer's Pick is: " + computer_choice + "\n")
+
+    if player_choice == computer_choice:
+        print("The pick is a draw!")
+    elif player_choice == "Rock":
+        if computer_choice == "Scissors":
             print("You Win!")
         else:
             print("You Lose!")
-    elif People == "Paper":
-        if Computer == "Rock":
+    elif player_choice == "Paper":
+        if computer_choice == "Rock":
             print("You Win!")
         else:
-            print("You Lose!" )
-    elif People == "Paper":
-        if Computer == "Scissors":
-            print("You Lose")
+            print("You Lose!")
+    elif player_choice == "Scissors":
+        if computer_choice == "Rock":
+            print("You Lose!")
         else:
-            print("You Win")
-    elif People == "Scissors":
-        if Computer == "Rock":
-            print("You Lose")
-        else:
-            print("You Win")
-    else: 
+            print("You Win!")
+    else:
         print("Invalid Option")
 
-    Exit()
+def main():
+    # Set up argparse to handle command-line arguments
+    parser = argparse.ArgumentParser(description="Play Rock Paper Scissors")
+    parser.add_argument('--choice', type=str, required=True, help='Your choice: Rock, Paper, or Scissors')
 
-def Exit():
+    args = parser.parse_args()
     
-    
-    
-    User = input("\nYou want to Continue Press 0 and 1 to Exit: ")
-    
-    if User == "0":
-        Main()
-    elif User == "1":
-        print("\nThank you for playing Bato Bato Pick")
-        quit()
-        
-        
-        
-    
+    play_game(args.choice)
 
-    
-        
-        
-    
-    
 if __name__ == "__main__":
-    Main()
-    Exit()
-    
+    main()
